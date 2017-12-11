3bc0b51556d6c5fab58a8f3aad7950fd25f14d6e
Ruud Verbeek <ruud@arfie.nl>
2017-12-11 20:58:55 +0100

    fix error in maphandler and add basic handler saving a tile array

--- tagpro_eu/handlers/map.py
+++ tagpro_eu/handlers/map.py
@@ -4,4 +4,15 @@ from tagpro_eu import constants
 class MapHandler:
     # flake8: noqa
     def height(self, new_y): pass
-    def tile(self, new_x, y): pass
+    def tile(self, new_x, y, tile): pass
+
+
+class MapSaver(MapHandler):
+    def __init__(self):
+        self.tiles = []
+
+    def height(self, y):
+        self.tiles.append([])
+
+    def tile(self, x, y, tile):
+        self.tiles[y].append(tile)
