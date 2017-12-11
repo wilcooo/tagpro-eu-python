13775f9779e795d8a5f98134c8d35c59150dc4fd
Ruud Verbeek <ruud@arfie.nl>
2017-12-11 18:18:29 +0100

    basics for JSON objects reading

---
+++ match.py
@@ -0,0 +1,73 @@
+import datetime
+
+
+class Blob:
+    def __init__(self, x):
+        pass
+
+
+class JsonObject:
+    def __init__(self, data):
+        for f, t in self.__class__.fields.items():
+            value = data.get(f, None)
+
+            if value is not None:
+                value = t(value)
+
+            setattr(self, f, value)
+
+
+class Map(JsonObject):
+    fields = {
+        'name': str,
+        'author': str,
+        'type': str,        # ctf, nf, mb etc., empty for unofficial maps
+        'marsballs': int,   # number
+        'width': int,
+        'tiles': Blob,
+    }
+
+
+class Player(JsonObject):
+    fields = {
+        'auth': bool,
+        'name': str,
+        'flair': int,   # index
+        'degree': int,
+        'score': int,
+        'points': int,  # rank points
+        'team': int,    # at start of match; 1 = red, 2 = blue, 0 = join late
+        'events': Blob,
+    }
+
+
+class Team(JsonObject):
+    fields = {
+        'name': str,
+        'score': int,
+        'splats': Blob
+    }
+
+
+def ListOf(t):
+    def inner(objs):
+        return list(map(t, objs))
+
+    return inner
+
+
+class Match(JsonObject):
+    fields = {
+        'server': str,              # Domain name of the game server
+        'port': int,
+        'official': bool,           # Played on an "official" server
+        'group': str,               # Group ID
+        # Start of the match
+        'date': datetime.datetime.fromtimestamp,
+        'timeLimit': int,           # In minutes
+        'duration': int,            # In frames
+        'finished': bool,
+        'map': Map,                 # Map object
+        'players': ListOf(Player),  # Array of player objects
+        'teams': ListOf(Team),      # Array of team objects
+    }
