7bbba047317aa97c4f5526cd27fbd89e7c86c280
Ruud Verbeek <ruud@arfie.nl>
2017-12-26 22:12:24 +0100

    remove SplatsLogger because its kinda useless

--- tagpro_eu/readers/splats.py
+++ tagpro_eu/readers/splats.py
@@ -8,9 +8,3 @@ class SplatsReader(SplatsHandler):
 
     def splats(self, splats, index):
         self.splatlist.extend(splats)
-
-
-class SplatsLogger(SplatsHandler):
-    def splats(self, splats, index):
-        for x, y in splats:
-            print(f'{index}: ({x}, {y})')
