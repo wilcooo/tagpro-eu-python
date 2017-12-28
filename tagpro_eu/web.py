127f2118c24c491bb160aab55e87d88024911740
Ruud Verbeek <ruud@arfie.nl>
2017-12-28 15:30:35 +0100

    add url and raw_url params to download_match

--- tagpro_eu/web.py
+++ tagpro_eu/web.py
@@ -4,10 +4,23 @@ import requests
 import tagpro_eu.core
 
 
-def download_match(id):
+def download_match(id=None, url=None, raw_url=None):
     """
-    Download the match from tagpro.eu with the given ID, and return it as a
-    Match object.
+    Download the match with the given ID or URL, and return it as a Match
+    object.
+
+    - If id is given, the match will be downloaded from tagpro.eu
+    - If url is given, the match ID will be extracted from it
+      - If this succeeds, the match with given ID will be downloaded from
+        tagpro.eu
+      - Otherwise, it will attempt to directly download the data from url,
+        or raw_url if it was given
+    - If raw_url is given, the data will be directly downloaded from there
+
+    Note that a single match ID as url will also succeed. If you take match
+    URLs as user input, you can usually just pass it as url parameter, which
+    will work for match links, download links, match IDs and raw data links
+    for matches hosted elsewhere.
 
     This method should probably not be called too many times, as it makes
     requests to tagpro.eu. If you have to download multiple matches, consider
@@ -15,10 +28,28 @@ def download_match(id):
     https://tagpro.eu/?science. The module tagpro_eu.core has methods for
     loading those.
 
-    :param id: the match ID
+    :param id: a match ID
+    :param url: a tagpro.eu match URL
+    :param raw_url: a direct link to raw data that doesn't have to be on
+    tagpro.eu
     :returns: the corresponding Match object
+    :raises ValueError: when no valid id, url or raw_url was given
     """
-    r = requests.get(f'https://tagpro.eu/?download={id}')
+    if id is None and url is not None:
+        id = match_url_to_id(url)
+
+        # if match_url_to_id didn't work and raw_url isn't set already,
+        # we will use url as raw_url
+        if raw_url is None:
+            raw_url = url
+
+    if id is not None:
+        raw_url = f'https://tagpro.eu/?download={id}'
+
+    if raw_url is None:
+        raise ValueError("No valid match ID or URL was given")
+
+    r = requests.get(raw_url)
     return tagpro_eu.core.Match(r.json())
 
 
