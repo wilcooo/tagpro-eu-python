54860fa9ddf540b2375da19a353d7ca6e2eb2ab9
Ruud Verbeek <ruud@arfie.nl>
2018-01-26 20:17:04 +0100

    Fix from_file and from_string methods

--- tagpro_eu/json.py
+++ tagpro_eu/json.py
@@ -1,5 +1,5 @@
 import datetime
-import json
+import json as _json
 
 from tagpro_eu.blob import Blob
 
@@ -62,7 +62,7 @@ class JsonObject:
         :raises TypeError: when strict mode is disabled and an element has the
         wrong data type
         """
-        return cls(json.loads(s), strict=strict)
+        return cls(_json.loads(s), strict=strict)
 
     @classmethod
     def from_file(cls, filename, strict=False):
@@ -78,7 +78,7 @@ class JsonObject:
         wrong data type
         """
         with open(filename) as f:
-            return cls(json.load(f), strict=strict)
+            return cls(_json.load(f), strict=strict)
 
     def __eq__(self, other):
         for f in self.__fields__.keys():
