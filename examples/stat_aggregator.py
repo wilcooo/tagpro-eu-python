48a810f3e444f9743b67f101398ac30dc9219837
Ruud Verbeek <ruud@arfie.nl>
2018-03-20 11:32:51 +0100

    Add stat aggregator example

---
+++ examples/stat_aggregator.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+Takes one or more TagPro Analytics matches through standard input, then prints
+a CSV file containing aggregated statistics for all players in those games.
+"""
+
+import tagpro_eu.player
+import tagpro_eu.web
+
+import csv
+import sys
+
+from collections import defaultdict
+
+
+links = []
+inp = input()
+while inp:
+    links.append(inp)
+    inp = input()
+
+stats = defaultdict(tagpro_eu.player.PlayerStats)
+
+for link in links:
+    match = tagpro_eu.web.download_match(link)
+    for player in match.players:
+        stats[player.name] += player.stats
+
+columns = [
+    ('time', lambda s: s.time // 60),
+    ('tags', lambda s: s.tags),
+    ('pops', lambda s: s.pops),
+    ('grabs', lambda s: s.grabs),
+    ('drops', lambda s: s.drops),
+    ('hold', lambda s: s.hold // 60),
+    ('captures', lambda s: s.captures),
+    ('returns', lambda s: s.returns),
+    ('prevent', lambda s: s.prevent // 60),
+    ('pups', lambda s: sum(s.pups.values())),
+    ('button', lambda s: s.button // 60),
+    ('block', lambda s: s.block // 60),
+]
+
+wr = csv.writer(sys.stdout)
+wr.writerow(['player'] + [t[0] for t in columns])
+
+for p, s in stats.items():
+    wr.writerow([p] + [t[1](s) for t in columns])
